0046f995afcea4ca5afd81bdaac45675a9c23feb
Sylvie-B <unsolo@hotmail.fr>
2021-12-21 22:44:32 +0100

    auto play by press x

--- app.js
+++ app.js
@@ -1,16 +1,54 @@
-// $('.key').eq(0).click(function (){
-//     $('audio').get(1).play();
-//
-// })
-
-// listen click on data truc get truc = bidul
-// so play audio data truc = bidul
-
+// get div with dataset
 let elem = $('div[data-key]');
-elem.click(function (){
 
+// listen click on div
+elem.click(function (){
+    // get dataset value
     let nbr = $(this).get(0).dataset.key;
-    console.log(nbr);
+    // play dataset sound = nbr
     $('audio[data-key='+ nbr + ']').get(0).play();
 })
 
+// get letter in kbd
+let kbd = $('kbd');
+
+let test = $('body');
+
+test.keydown(function (event){
+    // get key press
+    let alpha = event.key.toUpperCase();
+
+    kbd.each(function (){
+        if($(this).text() === alpha){
+            let nbr = $(this).parent().get(0).dataset.key;
+            $('audio[data-key='+ nbr + ']').get(0).play();
+        }
+    })
+})
+
+let word = "QSDFGHJKL";
+
+test.keydown(function (event){
+    if(event.key === 'x'){
+        console.log(event.key);
+        goOne(word);
+    }
+})
+
+function goOne (param){
+    let i = 0;
+    let t = 0;
+    let autoPlay = setInterval(function (){
+        if(i < 30){
+            let l = Math.floor(Math.random() * param.length);
+            kbd.each(function (){
+                if($(this).text() === param[l]){
+                    let nbr = $(this).parent().get(0).dataset.key;
+                    $('audio[data-key='+ nbr + ']').get(0).play();
+                }
+            })
+            i++;
+        }
+        else clearInterval(autoPlay);
+    }, 200)
+}
